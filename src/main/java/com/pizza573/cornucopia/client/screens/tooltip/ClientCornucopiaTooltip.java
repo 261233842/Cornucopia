28f51089d51394db04fdc6e51e3c4cba8952058f
13974 <13974729430@163.com>
2025-01-22 18:32:04 +0800

    build: 添加 Apple Skin 和 JEI(未启用) 依赖并更新相关配置
    - 在 build.gradle 中添加 Apple Skin 和 JEI 的 Maven 仓库
    - 在 build.gradle 中添加 Apple Skin 和 JEI 的 compileOnly 和 runtimeOnly 依赖
    - 在 gradle.properties 中添加 mc_version、jei_version 和 appleSkin_version属性

--- src/main/java/com/pizza573/cornucopia/client/screens/tooltip/ClientCornucopiaTooltip.java
+++ src/main/java/com/pizza573/cornucopia/client/screens/tooltip/ClientCornucopiaTooltip.java
@@ -12,6 +12,7 @@ import net.neoforged.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.math.Fraction;
 import org.jetbrains.annotations.NotNull;
 
+// todo tooltip貌似不用单独写，目前调用原版的也行
 @OnlyIn(Dist.CLIENT)
 public class ClientCornucopiaTooltip implements ClientTooltipComponent
 {
