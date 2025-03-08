3ffc148d5da7eb5145eca4ea85ba85d07423cb0c
13974 <13974729430@163.com>
2025-03-08 16:52:29 +0800

    refactor(cornucopia):重构 InstrumentItemMixin 类
    - 大致还原山羊角的功能
    - 重新命名了多个方法，添加了 mixin_ 前缀以区分原始方法
    - 优化了物品存储逻辑，支持单个物品堆叠超过 64 个
    - 调整了物品权重和存储容量，将最大权重从 64 增加到 128
    - 移除了未使用的代码和注释，简化了类结构
    - 更新了与新逻辑相关的音效和提示文本

--- src/main/java/com/pizza573/cornucopia/mixin/InstrumentItemMixin.java
+++ src/main/java/com/pizza573/cornucopia/mixin/InstrumentItemMixin.java
@@ -1,20 +1,16 @@
 package com.pizza573.cornucopia.mixin;
 
+import com.pizza573.cornucopia.init.ModItems;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.stats.Stats;
 import net.minecraft.util.Mth;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.SlotAccess;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
@@ -37,22 +33,20 @@ import java.util.stream.Stream;
 @Mixin(InstrumentItem.class)
 public class InstrumentItemMixin extends Item
 {
-    //    private static final String TAG_ITEMS = "Items";
-/*    @Unique
-    private static final int MAX_WEIGHT = 64;
+    // private static final String TAG_ITEMS = "Items";
     @Unique
-    private static final int BUNDLE_IN_BUNDLE_WEIGHT = 4;
+    private static final int MAX_WEIGHT = 128;
     @Unique
-    private static final int BAR_COLOR = Mth.color(0.4F, 0.4F, 1.0F);*/
+    private static final int BAR_COLOR = Mth.color(0.4F, 0.4F, 1.0F);
 
     public InstrumentItemMixin(Item.Properties pProperties)
     {
         super(pProperties);
     }
-/*
 
-    // 存储相关基本逻辑 start
-    private static int add(ItemStack pBundleStack, ItemStack pInsertedStack)
+    // 存储相关基本逻辑 ***start***
+    @Unique
+    private static int mixin_add(ItemStack pBundleStack, ItemStack pInsertedStack)
     {
         if (!pInsertedStack.isEmpty() && pInsertedStack.getItem().canFitInsideContainerItems()) {
             CompoundTag compoundtag = pBundleStack.getOrCreateTag();
@@ -60,26 +54,41 @@ public class InstrumentItemMixin extends Item
                 compoundtag.put("Items", new ListTag());
             }
 
-            int i = getContentWeight(pBundleStack);
-            int j = getWeight(pInsertedStack);
-            int k = Math.min(pInsertedStack.getCount(), (64 - i) / j);
+            int i = mixin_getContentWeight(pBundleStack);// 获取容器内物品总权重
+            int j = mixin_getWeight(pInsertedStack);// 获取插入单个物品的权重
+            int k = Math.min(pInsertedStack.getCount(), (MAX_WEIGHT - i) / j);// 此次插入物品的数量
             if (k == 0) {
                 return 0;
             } else {
                 ListTag listtag = compoundtag.getList("Items", 10);
-                Optional<CompoundTag> optional = getMatchingItem(pInsertedStack, listtag);
-                if (optional.isPresent()) {
-                    CompoundTag compoundtag1 = optional.get();
-                    ItemStack itemstack = ItemStack.of(compoundtag1);
-                    itemstack.grow(k);
-                    itemstack.save(compoundtag1);
-                    listtag.remove(compoundtag1);
-                    listtag.add(0, (Tag) compoundtag1);
-                } else {
+                Optional<CompoundTag> optional = mixin_getMatchingItem(pInsertedStack, listtag);// 寻找拥有相同tag的Item（匹配的itemStack）
+                if (optional.isPresent()) {// 找到匹配的物品
+                    CompoundTag compoundTag1 = optional.get();// compoundTag1
+                    ItemStack itemstack = ItemStack.of(compoundTag1);// 匹配的itemStack
+                    if (k + itemstack.getCount() > 64) {
+                        int increment = 64 - itemstack.getCount();
+                        int rest = k - increment;
+                        // grow
+                        itemstack.grow(increment);
+                        itemstack.save(compoundTag1);
+                        listtag.remove(compoundTag1);
+                        listtag.add(0, compoundTag1);
+                        // new CompoundTag
+                        ItemStack itemstack1 = pInsertedStack.copyWithCount(rest);
+                        CompoundTag compoundTag2 = new CompoundTag();
+                        itemstack1.save(compoundTag2);
+                        listtag.add(0, compoundTag2);
+                    } else if (k + itemstack.getCount() <= 64) {
+                        itemstack.grow(k);
+                        itemstack.save(compoundTag1);
+                        listtag.remove(compoundTag1);
+                        listtag.add(0, compoundTag1);
+                    }
+                } else {// 未找到匹配的物品
                     ItemStack itemstack1 = pInsertedStack.copyWithCount(k);
-                    CompoundTag compoundtag2 = new CompoundTag();
-                    itemstack1.save(compoundtag2);
-                    listtag.add(0, (Tag) compoundtag2);
+                    CompoundTag compoundTag2 = new CompoundTag();
+                    itemstack1.save(compoundTag2);
+                    listtag.add(0, compoundTag2);
                 }
 
                 return k;
@@ -89,17 +98,19 @@ public class InstrumentItemMixin extends Item
         }
     }
 
-    private static Optional<CompoundTag> getMatchingItem(ItemStack pStack, ListTag pList)
+    @Unique
+    private static Optional<CompoundTag> mixin_getMatchingItem(ItemStack pStack, ListTag pList)
     {
-        return pStack.is(Items.BUNDLE) ? Optional.empty() : pList.stream().filter(CompoundTag.class::isInstance).map(CompoundTag.class::cast).filter((p_186350_) -> {
-            return ItemStack.isSameItemSameTags(ItemStack.of(p_186350_), pStack);
+        return pList.stream().filter(CompoundTag.class::isInstance).map(CompoundTag.class::cast).filter((compoundTag) -> {
+            return ItemStack.isSameItemSameTags(ItemStack.of(compoundTag), pStack);
         }).findFirst();
     }
 
-    private static int getWeight(ItemStack pStack)
+    @Unique
+    private static int mixin_getWeight(ItemStack pStack)
     {
         if (pStack.is(Items.BUNDLE)) {
-            return 4 + getContentWeight(pStack);
+            return 4 + mixin_getContentWeight(pStack);
         } else {
             if ((pStack.is(Items.BEEHIVE) || pStack.is(Items.BEE_NEST)) && pStack.hasTag()) {
                 CompoundTag compoundtag = BlockItem.getBlockEntityData(pStack);
@@ -112,14 +123,14 @@ public class InstrumentItemMixin extends Item
         }
     }
 
-    private static int getContentWeight(ItemStack pStack)
+    @Unique
+    private static int mixin_getContentWeight(ItemStack pStack)
     {
-        return getContents(pStack).mapToInt((p_186356_) -> {
-            return getWeight(p_186356_) * p_186356_.getCount();
-        }).sum();
+        return mixin_getContents(pStack).mapToInt((itemStack) -> mixin_getWeight(itemStack) * itemStack.getCount()).sum();
     }
 
-    private static Optional<ItemStack> removeOne(ItemStack pStack)
+    @Unique
+    private static Optional<ItemStack> mixin_removeOne(ItemStack pStack)
     {
         CompoundTag compoundtag = pStack.getOrCreateTag();
         if (!compoundtag.contains("Items")) {
@@ -142,28 +153,8 @@ public class InstrumentItemMixin extends Item
         }
     }
 
-    private static boolean dropContents(ItemStack pStack, Player pPlayer)
-    {
-        CompoundTag compoundtag = pStack.getOrCreateTag();
-        if (!compoundtag.contains("Items")) {
-            return false;
-        } else {
-            if (pPlayer instanceof ServerPlayer) {
-                ListTag listtag = compoundtag.getList("Items", 10);
-
-                for (int i = 0; i < listtag.size(); ++i) {
-                    CompoundTag compoundtag1 = listtag.getCompound(i);
-                    ItemStack itemstack = ItemStack.of(compoundtag1);
-                    pPlayer.drop(itemstack, true);
-                }
-            }
-
-            pStack.removeTagKey("Items");
-            return true;
-        }
-    }
-
-    private static Stream<ItemStack> getContents(ItemStack pStack)
+    @Unique
+    private static Stream<ItemStack> mixin_getContents(ItemStack pStack)
     {
         CompoundTag compoundtag = pStack.getTag();
         if (compoundtag == null) {
@@ -173,9 +164,8 @@ public class InstrumentItemMixin extends Item
             return listtag.stream().map(CompoundTag.class::cast).map(ItemStack::of);
         }
     }
-    // 存储相关基本逻辑 end
-*/
-/*
+    // 存储相关基本逻辑 ***end***
+
     @Override
     public boolean overrideStackedOnOther(ItemStack pStack, @NotNull Slot pSlot, @NotNull ClickAction pAction, @NotNull Player pPlayer)
     {
@@ -184,15 +174,15 @@ public class InstrumentItemMixin extends Item
         } else {
             ItemStack itemstack = pSlot.getItem();
             if (itemstack.isEmpty()) {
-                this.playRemoveOneSound(pPlayer);
-                removeOne(pStack).ifPresent((p_150740_) -> {
-                    add(pStack, pSlot.safeInsert(p_150740_));
+                this.mixin_playRemoveOneSound(pPlayer);
+                mixin_removeOne(pStack).ifPresent((itemStack) -> {
+                    mixin_add(pStack, pSlot.safeInsert(itemStack));
                 });
-            } else if (itemstack.getItem().canFitInsideContainerItems()) {
-                int i = (64 - getContentWeight(pStack)) / getWeight(itemstack);
-                int j = add(pStack, pSlot.safeTake(itemstack.getCount(), i, pPlayer));
+            } else if (itemstack.getItem().canFitInsideContainerItems() && itemstack.getFoodProperties(pPlayer) != null) {
+                int i = (MAX_WEIGHT - mixin_getContentWeight(pStack)) / mixin_getWeight(itemstack);
+                int j = mixin_add(pStack, pSlot.safeTake(itemstack.getCount(), i, pPlayer));
                 if (j > 0) {
-                    this.playInsertSound(pPlayer);
+                    this.mixin_playInsertSound(pPlayer);
                 }
             }
 
@@ -206,14 +196,14 @@ public class InstrumentItemMixin extends Item
         if (pStack.getCount() != 1) return false;
         if (pAction == ClickAction.SECONDARY && pSlot.allowModification(pPlayer)) {
             if (pOther.isEmpty()) {
-                removeOne(pStack).ifPresent((p_186347_) -> {
-                    this.playRemoveOneSound(pPlayer);
-                    pAccess.set(p_186347_);
+                mixin_removeOne(pStack).ifPresent((itemStack) -> {
+                    this.mixin_playRemoveOneSound(pPlayer);
+                    pAccess.set(itemStack);
                 });
-            } else {
-                int i = add(pStack, pOther);
+            } else if (pOther.getFoodProperties(pPlayer) != null) {
+                int i = mixin_add(pStack, pOther);
                 if (i > 0) {
-                    this.playInsertSound(pPlayer);
+                    this.mixin_playInsertSound(pPlayer);
                     pOther.shrink(i);
                 }
             }
@@ -225,26 +215,25 @@ public class InstrumentItemMixin extends Item
     }
 
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level pLevel, Player pPlayer, InteractionHand pUsedHand)
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, @NotNull Level level, @NotNull LivingEntity livingEntity)
     {
-        ItemStack itemstack = pPlayer.getItemInHand(pUsedHand);
-        if (dropContents(itemstack, pPlayer)) {
-            this.playDropContentsSound(pPlayer);
-            pPlayer.awardStat(Stats.ITEM_USED.get(this));
-            return InteractionResultHolder.sidedSuccess(itemstack, pLevel.isClientSide());
-        } else {
-            return InteractionResultHolder.fail(itemstack);
+        if (mixin_getContentWeight(stack) == MAX_WEIGHT) {// 装满食物
+            // todo 适配“是否清空食物”配置
+            ItemStack cornucopiaItemStack = new ItemStack(ModItems.CORNUCOPIA.get());
+            return cornucopiaItemStack;
         }
+
+        return stack;
     }
 
     public boolean isBarVisible(@NotNull ItemStack pStack)
     {
-        return getContentWeight(pStack) > 0;
+        return mixin_getContentWeight(pStack) > 0;
     }
 
     public int getBarWidth(@NotNull ItemStack pStack)
     {
-        return Math.min(1 + 12 * getContentWeight(pStack) / 64, 13);
+        return Math.min(1 + 12 * mixin_getContentWeight(pStack) / MAX_WEIGHT, 13);
     }
 
     public int getBarColor(@NotNull ItemStack pStack)
@@ -252,44 +241,30 @@ public class InstrumentItemMixin extends Item
         return BAR_COLOR;
     }
 
-
-
-
     public @NotNull Optional<TooltipComponent> getTooltipImage(@NotNull ItemStack pStack)
     {
         NonNullList<ItemStack> nonnulllist = NonNullList.create();
-        getContents(pStack).forEach(nonnulllist::add);
-        return Optional.of(new BundleTooltip(nonnulllist, getContentWeight(pStack)));
-    }*/
+        mixin_getContents(pStack).forEach(nonnulllist::add);
+        return Optional.of(new BundleTooltip(nonnulllist, mixin_getContentWeight(pStack)));
+    }
 
     @Inject(method = "appendHoverText", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/InstrumentItem;getInstrument(Lnet/minecraft/world/item/ItemStack;)Ljava/util/Optional;"))
     public void appendHoverTextInject(ItemStack pStack, Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced, CallbackInfo info)
     {
-        pTooltipComponents.add(Component.translatable("aaaaaaaaaaaaaaaa"));
-        System.out.println("-----------------appendHoverTextInject--------------------------------------------");
-//        pTooltipComponents.add(Component.translatable("item.minecraft.bundle.fullness", getContentWeight(pStack), 64).withStyle(ChatFormatting.GRAY));
-    }
-/*
-
-    public void onDestroyed(@NotNull ItemEntity pItemEntity)
-    {
-        ItemUtils.onContainerDestroyed(pItemEntity, getContents(pItemEntity.getItem()));
+        pTooltipComponents.add(Component.translatable("item.minecraft.bundle.fullness", mixin_getContentWeight(pStack), 128).withStyle(ChatFormatting.GRAY));
+        pTooltipComponents.add(Component.translatable("item.minecraft.goat_horn.description").withStyle(ChatFormatting.GRAY));
     }
 
-    private void playRemoveOneSound(Entity pEntity)
+    // 相关音效*2
+    @Unique
+    private void mixin_playRemoveOneSound(Entity pEntity)
     {
         pEntity.playSound(SoundEvents.BUNDLE_REMOVE_ONE, 0.8F, 0.8F + pEntity.level().getRandom().nextFloat() * 0.4F);
     }
 
-    private void playInsertSound(Entity pEntity)
+    @Unique
+    private void mixin_playInsertSound(Entity pEntity)
     {
         pEntity.playSound(SoundEvents.BUNDLE_INSERT, 0.8F, 0.8F + pEntity.level().getRandom().nextFloat() * 0.4F);
     }
-
-    private void playDropContentsSound(Entity pEntity)
-    {
-        pEntity.playSound(SoundEvents.BUNDLE_DROP_CONTENTS, 0.8F, 0.8F + pEntity.level().getRandom().nextFloat() * 0.4F);
-    }
-*/
-
 }
